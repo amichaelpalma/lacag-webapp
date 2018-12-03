049479961e8e2e1efce7d95c29ecf64cd71700fa
amichaelpalma <amichaelpalma@gmail.com>
2018-12-03 13:46:52 +0800

    add index, new & create in donations controller, add _form & _messages and gitignore DS_Store

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,3 +1,29 @@
 class DonationsController < ApplicationController
 
+  def index
+    @donations = Donation.with_attached_item_photo
+  end
+
+  def new
+    @donation = Donation.new
+  end
+
+  def create
+    @donation = Donation.new(donation_params)
+
+    if @donation.save
+      flash[:success] = "A donation item was added to the list."
+      redirect_to donations_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:item_name, :item_photo, :item_description)
+  end
+
 end
