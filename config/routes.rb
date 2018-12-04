832cbeb58f7d44a1783b2f495da440104a233656
amichaelpalma <amichaelpalma@gmail.com>
2018-12-04 20:20:33 +0800

    root donations#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'donations#index'
   resources 'donations'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
