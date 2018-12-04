140f369868b00b222b3579e71810fffe305ccbe9
amichaelpalma <amichaelpalma@gmail.com>
2018-12-04 17:04:39 +0800

    add update & delete ability to donations and update show and index page

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,7 @@
 class DonationsController < ApplicationController
 
+  before_action :set_donation, only: [:edit, :update, :show, :destroy]
+
   def index
     @donations = Donation.with_attached_item_photo
   end
@@ -19,11 +21,36 @@ class DonationsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @donation.update(donation_params)
+      flash[:notice] = "Item successfully updated."
+      redirect_to donation_path(@donation)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @donation.destroy
+    flash[:notice] = "Donation item was successfully deleted."
+    redirect_to donations_path
+  end
+
 
   private
 
   def donation_params
     params.require(:donation).permit(:item_name, :item_photo, :item_description)
+  end
+
+  def set_donation
+    @donation = Donation.find(params[:id])
   end
 
 end
