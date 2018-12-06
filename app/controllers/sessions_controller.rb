f11f7d42f0098e75aa1381e6bef18f7679a7b9d4
amichaelpalma <amichaelpalma@gmail.com>
2018-12-06 22:58:02 +0800

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to donations_path
+    else
+      flash[:danger] = "Email/Password combination is incorrect. Try again or contact your admin."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to donations_path
+  end
+
+end
