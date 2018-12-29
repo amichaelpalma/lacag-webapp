5097a3d484c3cd6a5256ff605b2ef0b6864b5721
amichaelpalma <amichaelpalma@gmail.com>
2018-12-29 12:39:42 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   # Pages
   get '/about', to: "pages#about"
   get '/seminarsandtrainings', to: "pages#seminarsandtrainings"
-<<<<<<< HEAD
   get '/secret', to: "pages#secret"
   get '/donate', to: "pages#donate"
-=======
-
->>>>>>> 8-add-list-for-seminarsandtrainings-request
 
   #Request for Seminar and Training
   resources 'requests'
